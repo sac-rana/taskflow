ea6ff9e35ee6aff404ac2570ca9fe36275c5f9e4
Sachin Rana <sachinrana1827@gmail.com>
2023-11-02 16:36:30 +0530

    added "project" and "task" model in app "projects"

--- backend/projects/admin.py
+++ backend/projects/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Project, Task
 
 # Register your models here.
+
+
+class TaskInline(admin.TabularInline):
+    model = Task
+    extra = 1
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "completed", "created_at")
+
+    inlines = [TaskInline]
+
+    class Meta:
+        model = Project
+
+
+admin.site.register(Project, ProjectAdmin)
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+class Project(models.Model):
+    user = models.ForeignKey(
+        get_user_model(), related_name="projects", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Task(models.Model):
+    project = models.ForeignKey(Project, related_name="tasks", on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
