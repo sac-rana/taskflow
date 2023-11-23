b887a26663004188f652ba62ef3cc7839185c10a
Sachin Rana <sachinrana1827@gmail.com>
2023-11-23 16:09:34 +0530

    ui update

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         aria-label='Global'
       >
         <div className='flex lg:flex-1'>
-          <Link href='#' className='-m-1.5 p-1.5'>
+          <Link href='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>Your Company</span>
             <img
               className='h-8 w-auto'
@@ -88,7 +88,7 @@ const Header = () => {
           } fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10`}
         >
           <div className='flex items-center justify-between'>
-            <Link href='#' className='-m-1.5 p-1.5'>
+            <Link href='/' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your Company</span>
               <img
                 className='h-8 w-auto'
